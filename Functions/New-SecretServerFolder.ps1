8e9bbf633f51030d9be5e5a2c8d6c0afde988a76
dnlrv <kaisenai@gmail.com>
2023-08-31 12:33:36 -0400

    Added SecretServerFolder parameter option to New-SecretServerFolder

--- Functions/New-SecretServerFolder.ps1
+++ Functions/New-SecretServerFolder.ps1
@@ -8,10 +8,11 @@ function global:New-SecretServerFolder
     Creates a new Secret Server Folder.
 
     .DESCRIPTION
-	This function creates a new Secret Serfer Folder in the connected Secret Server instance. By default this function
+	This function creates a new Secret Server Folder in the connected Secret Server instance. By default this function
 	will attempt to create a new Root level folder if just the name is specified.
 
-	Otherwise, the Parent Folder can be specified as a string provided with the -ParentFolderPath parameter.
+	Otherwise, the Parent Folder can be specified as a string provided with the -ParentFolderPath parameter. A 
+	SecretServerFolder class object is required for the SecretServerFolder parameter.
 
 	This function returns a SecretServerFolder class object if successful. This function will return $false if a new
 	folder was not created.
@@ -29,6 +30,9 @@ function global:New-SecretServerFolder
 	.PARAMETER ParentFolderPath
 	The Folder Path name of the desired Parent Folder.
 
+	.PARAMETER SecretServerFolder
+	The SecretServerFolder class object to serve as the Parent Folder.
+
 	.PARAMETER InheritPermissions
 	Should permissions be inherited from the Parent Folder. Default is true.
 
@@ -40,27 +44,34 @@ function global:New-SecretServerFolder
     This function will create a new Root-level Secret Server Folder called "My Team Folder".
 
 	.EXAMPLE
-	C:\PS> New-SecretServerFolder -Name "WidgetApp Root Accounts" -ParentPath "Apps\WidgetApp"
+	C:\PS> New-SecretServerFolder -Name "WidgetApp Root Accounts" -ParentFolderPath "Apps\WidgetApp"
 	This function will create a new Secret Server Folder in the "Apps\WidgetApps" directory named "WidgetApp Root Accounts."
 	
+	.EXAMPLE
+	C:\PS> New-SecretServerFolder -Name "BlueCrab Accounts" -SecretServerFolder (Get-SecretServerFolder -Name "BlueCrabApp")
+	This function will get the SecretServerFolder called "BlueCrabApp" and use that to serve as the Parent Folder for the 
+	new "BlueCrab Accounts" subfolder.
     #>
     [CmdletBinding(DefaultParameterSetName="RootFolder")]
     param
     (
         [Parameter(Mandatory = $true, Position = 0, HelpMessage = "The name of the Folder to create.", ParameterSetName = "RootFolder")]
 		[Parameter(Mandatory = $true, Position = 0, HelpMessage = "The name of the Folder to create.", ParameterSetName = "PathFolder")]
-        [System.String]$Name,
+        [Parameter(Mandatory = $true, Position = 0, HelpMessage = "The name of the Folder to create.", ParameterSetName = "SecretServerFolder")]
+		[System.String]$Name,
 
 		[Parameter(Mandatory = $true, Position = 1, HelpMessage = "The Folder Path of the Parent Folder.", ParameterSetName = "PathFolder")]
 		[System.String]$ParentFolderPath,
 
 		[Parameter(Mandatory = $true, Position = 1, HelpMessage = "The SecretServerFolder object to server as the Parent Folder.", ParameterSetName = "SecretServerFolder")]
-		[PSTypeName('SecretServerFolder')]$ParentFolder,
+		[PSTypeName('SecretServerFolder')]$SecretServerFolder,
 
 		[Parameter(Mandatory = $false, HelpMessage = "Should permissions inherit from parent folder.", ParameterSetName = "PathFolder")]
+		[Parameter(Mandatory = $false, HelpMessage = "Should permissions inherit from parent folder.", ParameterSetName = "SecretServerFolder")]
 		[System.Boolean]$InheritPermissions = $true,
 
 		[Parameter(Mandatory = $false, HelpMessage = "Should Secret policy inherit from parent folder.", ParameterSetName = "PathFolder")]
+		[Parameter(Mandatory = $false, HelpMessage = "Should Secret policy inherit from parent folder.", ParameterSetName = "SecretServerFolder")]
 		[System.Boolean]$InheritSecretPolicy = $true
     )
 
@@ -104,6 +115,29 @@ function global:New-SecretServerFolder
 				$_
 			}
 		}# "PathFolder"
+		"SecretServerFolder"
+		{
+			if (($SecretServerFolder | Measure-Object | Select-Object -ExpandProperty Count) -gt 1)
+			{
+				throw ("More than 1 Secret Server Folder provided. Please specify a single Secret Server Folder.")
+			}
+
+			Try
+			{
+				# preparing the body payload for a root folder creation
+				$BodyPayload.folderName          = $Name
+				$BodyPayload.parentFolderId      = $SecretServerFolder.ID
+				$BodyPayload.inheritPermissions  = $InheritPermissions
+				$BodyPayload.inheritSecretPolicy = $InheritSecretPolicy
+				break
+			}
+			Catch
+			{
+				# we'll eventually refine this
+				throw ("Something went wrong with SecretServerFolder")
+				$_
+			}
+		}# "SecretServerFolder"
 	}# Switch ($PSCmdlet.ParameterSetName)
 
 	# make the call and attempt to create the folder
